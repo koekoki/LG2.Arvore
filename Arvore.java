3b912c3361d53119d641bd010b22ffee2c64c8b9
Kaiky Matsumoto <47525176+koekoki@users.noreply.github.com>
2021-02-05 18:06:13 -0300

    Add files via upload

---
+++ Arvore.java
@@ -0,0 +1,62 @@
+package Arvore;
+
+public class Arvore<TIPO extends Comparable>{
+    private Elemento<TIPO> raiz;
+
+    public Arvore(){
+        this.raiz = null;
+    }
+
+    public void adicionar (TIPO valor){
+        Elemento<TIPO>novoElemento = new Elemento<TIPO>(valor);
+        if(raiz == null)
+            this.raiz = novoElemento;
+        else{
+            Elemento<TIPO> atual= this.raiz;
+            while (true){
+                if (novoElemento.getValor().compareTo(atual.getValor())==-1){
+                    if (atual.getEsquerda() != null){
+                        atual = atual.getEsquerda();
+                    }else{
+                        atual.setEsquerda(novoElemento);
+                        break;
+                    }
+                }else {/*se for maior ou igual*/
+                        if (atual.getDireita()!=null){
+                            atual = atual.getDireita();
+                        }else {
+                            atual.setDireita(novoElemento);
+                            break;
+                        }
+                }
+            }
+        }
+
+    }
+
+    public Elemento<TIPO> getRaiz() {
+        return raiz;
+    }
+
+    public void emOrdem(Elemento<TIPO> atual){
+        if (atual != null){
+            emOrdem(atual.getEsquerda());
+            System.out.print(" "+atual.getValor());
+            emOrdem(atual.getDireita());
+        }
+    }
+    public void preOrdem(Elemento<TIPO> atual){
+        if (atual != null){
+            System.out.print(" "+atual.getValor());
+            preOrdem(atual.getEsquerda());
+            preOrdem(atual.getDireita());
+        }
+    }
+    public void posOrdem(Elemento<TIPO> atual){
+        if (atual != null){
+            posOrdem(atual.getEsquerda());
+            posOrdem(atual.getDireita());
+            System.out.print(" "+atual.getValor());
+        }
+    }
+}
---
+++ Elemento.java
@@ -0,0 +1,38 @@
+package Arvore;
+
+public class Elemento<TIPO>{
+    private TIPO valor;
+    private Elemento<TIPO> esquerda;
+    private Elemento<TIPO> direita;
+
+    public Elemento(TIPO valor){
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+
+    }
+
+    public Elemento<TIPO> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(Elemento<TIPO> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public Elemento<TIPO> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(Elemento<TIPO> direita) {
+        this.direita = direita;
+    }
+
+    public TIPO getValor() {
+        return valor;
+    }
+
+    public void setValor(TIPO valor) {
+        this.valor = valor;
+    }
+}
